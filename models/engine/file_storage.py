c18d115e1af069eb0f6967fe6a3ac95138c0c541
b7sh <mohammedabdelbasit500@gmail.com>
2024-02-10 16:46:23 +0300

    update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -14,35 +14,35 @@ class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
-    def all(FileStorage):
-        return FileStorage.__objects
+    def all(self):
+        return self.__objects
 
-    def new(FileStorage, obj):
-        FileStorage.__objects[
+    def new(self, obj):
+        self.__objects[
             f"{obj.__class__.__name__}.{obj.id}"] = obj
 
-    def save(FileStorage):
+    def save(self):
         serilized_dict = {}
         try:
-            for key in FileStorage.__objects.keys():
-                serilized_dict[key] = FileStorage.__objects[key].to_dict()
-            with open(FileStorage.__file_path, "w") as f:
+            for key in self.__objects.keys():
+                serilized_dict[key] = self.__objects[key].to_dict()
+            with open(self.__file_path, "w") as f:
                 json.dump(serilized_dict, f)
         except FileNotFoundError:
             pass
 
-    def reload(FileStorage):
+    def reload(self):
         classes = {"BaseModel": BaseModel, "User": User, "City": City,
                    "Place": Place, "State": State, "Amenity": Amenity,
                    "Review": Review}
         try:
-            with open(FileStorage.__file_path, "r") as f:
+            with open(self.__file_path, "r") as f:
                 deserilized_dict = json.load(f)
             for key, value in deserilized_dict.items():
                 Base = value["__class__"]
                 Base = classes[value["__class__"]]
                 new_base = Base(**value)
-                FileStorage.new(new_base)
+                self.new(new_base)
 
         except FileNotFoundError:
             return
