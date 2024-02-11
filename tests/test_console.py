2b1099742d0b35149f7ef7d6e59d63206c9c8e94
b7sh <mohammedabdelbasit500@gmail.com>
2024-02-12 01:35:05 +0300

    update

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,1 +1,11 @@
+#!/usr/bin/python3
+"""the consol test cases"""
 import unittest
+from console import HBNBCommand
+
+
+class HBNBCommand_prompting(unittest.TestCase):
+    """the prompt test class"""
+    def test_prompt_type(self):
+        """the prompt type test"""
+        self.assertEqual("(hbnb) ", HBNBCommand.prompt)
