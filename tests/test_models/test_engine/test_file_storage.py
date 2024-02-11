3221934a08e79a8bcaf7fbd26cf8ddb9fd1ed0f9
b7sh <mohammedabdelbasit500@gmail.com>
2024-02-11 23:51:56 +0300

    update

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -3,6 +3,13 @@
 import unittest
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
+from models.user import User
+from models.city import City
+from models.place import Place
+from models.state import State
+from models.review import Review
+from models.amenity import Amenity
+import models
 
 
 class testattribute(unittest.TestCase):
@@ -48,3 +55,42 @@ class testmethods(unittest.TestCase):
         with self.assertRaisesRegex(AttributeError,
                                     "object has no attribute 'id'"):
             self.storage.new("obj")
+
+    def test_new(self):
+        base_model = BaseModel()
+        user = User()
+        city = City()
+        state = State()
+        place = Place()
+        review = Review()
+        amenity = Amenity()
+
+        models.storage.new(base_model)
+        models.storage.new(user)
+        models.storage.new(city)
+        models.storage.new(state)
+        models.storage.new(place)
+        models.storage.new(review)
+        models.storage.new(amenity)
+
+        self.assertIn("BaseModel." + base_model.id,
+                       models.storage.all().keys())
+        self.assertIn(base_model, models.storage.all().values())
+        self.assertIn("User." + user.id,
+                       models.storage.all().keys())
+        self.assertIn(user, models.storage.all().values())
+        self.assertIn("City." + city.id,
+                       models.storage.all().keys())
+        self.assertIn(city, models.storage.all().values())
+        self.assertIn("State." + state.id,
+                       models.storage.all().keys())
+        self.assertIn(state, models.storage.all().values())
+        self.assertIn("Place." + place.id,
+                       models.storage.all().keys())
+        self.assertIn(place, models.storage.all().values())
+        self.assertIn("Review." + review.id,
+                       models.storage.all().keys())
+        self.assertIn(review, models.storage.all().values())
+        self.assertIn("Amenity." + amenity.id,
+                       models.storage.all().keys())
+        self.assertIn(amenity, models.storage.all().values())
