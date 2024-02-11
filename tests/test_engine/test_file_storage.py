0ed5bce11b2fc7b8274dce6b72d347c42ee68a86
Omar-Algassim <omarggg2@gamil.com>
2024-02-11 05:04:28 +0200

    update

---
+++ tests/test_engine/test_file_storage.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+import unittest
+from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+
+
+class testattribute(unittest.TestCase):
+    """test class attribute"""
+
+    def setUp(self):
+        self.storage = FileStorage()
+        self.base = BaseModel()
+
+    def test_oject(self):
+        """test object attribute"""
+        objects = self.storage.all()
+        self.assertIsInstance(objects, dict)
+        self.assertIsNotNone(objects)
+
+    def test_new_no_arg(self):
+        """dont pass argument to new method"""
+        with self.assertRaisesRegex(TypeError,
+                                    " missing 1 required positional argument"):
+            self.storage.new()
+
+    def test_new_with_arg(self):
+        """pass obj to new method"""
+        self.storage.new(self.base)
+        objects = self.storage.all()
+        self.assertIn(f"{self.base.__class__.__name__}.{self.base.id}", objects)
